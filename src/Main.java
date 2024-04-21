e17e3b5a6c62c2d90e759551ea47b693a2c5a2db
Fullyverified <fullyverified1@gmail.com>
2024-04-21 16:35:31 +0930

    Add Camera and Vertice objects.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args)
     {
+        Camera cam = new Camera(0,0,0);
 
 
     }
--- src/Vertex.java
+++ src/Vertex.java
@@ -1,17 +1,11 @@
-public class Camera {
+public class Vertex {
 
     private double x;
     private double y;
     private double z;
 
-    public static void main(String[] args)
-    {
-
-    }
-
-
     //Constructor
-    public Camera(double x, double y, double z)
+    public Vertex (double x, double y, double z)
     {
         this.x = x;
         this.y = y;
