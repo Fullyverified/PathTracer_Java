3461c27324e5646576086dd84c71602f20542efb
Fullyverified <fullyverified1@gmail.com>
2024-04-23 12:03:46 +0930

    added more comments for clarity

--- src/Sphere.java
+++ src/Sphere.java
@@ -17,8 +17,6 @@ public class Sphere {
         this.sradius = sradius;
     }
 
-    // discriminant = b^2 - 2ac
-
     // p = o + td
     // p new ray position
     // o ray origin
@@ -35,13 +33,15 @@ public class Sphere {
         // oc = (vectorx, vectory, vectorz)
 
         // calculate values of a, b, c for the quadratic equation
-        // the normalised direction should always be 1 (so it doesn't matter if you use x, y or z)
+        // a = the dot product of normx, normy, normz - should always equal 1
         this.a = (ray.getNormDirX() * ray.getNormDirX()) + (ray.getNormDirY() * ray.getNormDirY() + (ray.getNormDirZ() * ray.getNormDirZ()));
+        // b = 2 * (the dot product of the centerorigin vector by the direction vector)
         this.b = 2 * ((centerOriginX * ray.getNormDirX()) + (centerOriginY * ray.getNormDirY()) + (centerOriginZ * ray.getNormDirZ()));
+        // c = the dot product of centerorigin by itself, - the radius^2 of the sphere
         this.c = ((centerOriginX * centerOriginX) + (centerOriginY * centerOriginY) + (centerOriginZ * centerOriginZ) - (this.sradius * this.sradius));
         System.out.println("a = " + a + ". b = " + b + ". c = " + c);
 
-        // calculate the discriminant
+        // calculate the discriminant | b^2 - 2ac
         this.discriminant = (b * b) - (4 * (a * c));
 
         if (this.discriminant < 0)
